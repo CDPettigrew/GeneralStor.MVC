b7f6f0e270cdb5f5c5c8a96baaab579fd1167d0c
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-26 12:07:30 -0400

    Customer Edit Added

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -56,6 +57,33 @@ namespace GeneralStore.MVC.Controllers
             _db.Customers.Remove(customer);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        //GET: Customer/Edit{id}
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        //POST: Customer/Edit{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(customer).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
         }
     }
 }
