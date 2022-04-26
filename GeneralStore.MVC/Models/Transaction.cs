9d658bba5d467e9c2e175c881f9ac2646f083788
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-26 13:08:39 -0400

    transaction model added

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(Customer))]
+        public virtual Customer Customer { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfTransaction { get; set; }
+        public decimal Total
+        {
+            get
+            {
+                if (Product == null)
+                    return 0;
+                decimal totalCost = Quantity * Product.Price;//get cost price times amount purchase
+                decimal tax = Decimal.Multiply(totalCost, 0.07m);// add sales tax after discount
+                return Math.Round(totalCost + tax, 2);//Round to the nearest .00 place
+            }
+        }
+    }
+}
